ee97ea0ba0251d3b9b11e2d95330cc2630f416d8
Jeff <jumbricht@sandstormdesign.com>
2017-02-24 14:30:57 -0600

    Added git hook integration for eslint

--- install.js
+++ install.js
@@ -1,3 +1,6 @@
 var Validate = require('git-validate');
 
 Validate.copy('eslintrc', '.eslintrc');
+
+Validate.installScript('eslint', 'eslint .');
+Validate.configureHook('pre-commit', ['eslint']);
